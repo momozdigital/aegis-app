567c522063625cb809eaf01a0803d2db5ce5d96c
Wilmer A <wrmomoz@gmail.com>
2021-06-03 15:34:33 -0400

    v1 complete

---
+++ public/precache-manifest.6d95918378776dbef9b63d2ba57c23c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3343cd095b0735e89ef20bb0ca8fe4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4fd9e694e5f523113f5",
+    "url": "/static/css/2.b1ca7953.chunk.css"
+  },
+  {
+    "revision": "be142228f80515fa4682",
+    "url": "/static/css/main.f035ba6c.chunk.css"
+  },
+  {
+    "revision": "c4fd9e694e5f523113f5",
+    "url": "/static/js/2.bd64b146.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd64b146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be142228f80515fa4682",
+    "url": "/static/js/main.53092bc7.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
